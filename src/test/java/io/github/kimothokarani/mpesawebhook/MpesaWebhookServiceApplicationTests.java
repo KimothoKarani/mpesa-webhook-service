5bcbe2ef00aecc3384ccc97f65d2734a07160e73
kimothokarani <kimothokarani@gmail.com>
2026-07-02 15:59:11 +0300

    Initial Spring Boot scaffold from Spring Initializr

---
+++ src/test/java/io/github/kimothokarani/mpesawebhook/MpesaWebhookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kimothokarani.mpesawebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaWebhookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
